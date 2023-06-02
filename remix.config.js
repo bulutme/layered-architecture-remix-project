c48392838f61456e95e8fc228f9e9c0311b486bd
bulutme <bltbey06@gmail.com>
2023-06-02 17:49:10 +0300

    build error fix

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  serverDependenciesToBundle: ["axios"],
   ignoredRouteFiles: ["**/.*"],
-
   serverModuleFormat: "cjs",
   future: {
     v2_errorBoundary: true,
